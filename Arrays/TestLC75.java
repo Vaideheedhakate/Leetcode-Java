d236109df54c6f8d1ec05846e75d6879f9fb50b0
Vaideheedhakate <vaidehedha@gmail.com>
2025-12-22 16:16:55 +0530

    Modified: Change test Case.

--- Arrays/TestLC75.java
+++ Arrays/TestLC75.java
@@ -3,19 +3,12 @@ import java.util.Arrays;
 public class TestLC75 {
     public static void main(String[] args) {
 
-        System.out.println("=== LC75 – Sort Colors ===");
-
         LC75_SortColors sol = new LC75_SortColors();
 
-        // Test Case 1
+        // Test Case
         int[] nums1 = {2,0,2,1,1,0};
         sol.sortColors(nums1);
         System.out.println(Arrays.toString(nums1));
 
-        // Test Case 2
-        int[] nums2 = {2,0,1};
-        sol.sortColors(nums2);
-        System.out.println(Arrays.toString(nums2));
-        
     }
 }
