0915a3717a8a085acb01d94c8b105f5157c15476
Vaideheedhakate <vaidehedha@gmail.com>
2025-12-22 16:10:53 +0530

    Solved : Leetcode 189 Qs.

---
+++ Arrays/LC189_RotateArray.java
@@ -0,0 +1,17 @@
+public class LC189_RotateArray {
+    public void reverse(int[] arr,int i,int j){
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++; j--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+}
