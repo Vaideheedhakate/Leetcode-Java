23b86b6c16219b2530cc2b387384acda69e89e72
Vaideheedhakate <vaidehedha@gmail.com>
2025-12-25 11:48:04 +0530

    Solved: Leetcode Qs no. 54

--- Arrays/LC54_SpiralMatrix.java
+++ Arrays/LC54_SpiralMatrix.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class LC54_SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] arr) {
+        int m = arr.length, n=arr[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int minr=0, maxr=m-1;
+        int minc=0, maxc=n-1;
+        while(minr<=maxr && minc<=maxc){
+            //left to right
+            for(int j=minc;j<=maxc;j++){
+                ans.add(arr[minr][j]);
+            } minr++;
+            if(minr>maxr || minc>maxc) break;
+            //top to bottom
+            for(int i=minr;i<=maxr;i++){
+                ans.add(arr[i][maxc]);
+            } maxc--;
+            if(minr>maxr || minc>maxc) break;
+            //right to left
+            for(int j=maxc;j>=minc;j--){
+                ans.add(arr[maxr][j]);
+            } maxr--;
+            if(minr>maxr || minc>maxc) break;
+            //bottom to top
+            for(int i=maxr;i>=minr;i--){
+                ans.add(arr[i][minc]);
+            } minc++;
+        }
+        return ans;
+    }
 }
