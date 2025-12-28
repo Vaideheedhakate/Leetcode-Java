a7c822677c4c0fd89c197e2a0282ed34c81ea977
Vaideheedhakate <vaidehedha@gmail.com>
2025-12-28 19:26:28 +0530

    docs: added explanatory comments for LC240 Search in 2D Matrix.

--- Arrays/LC240_SearchA2DMatrix.java
+++ Arrays/LC240_SearchA2DMatrix.java
@@ -1,13 +1,17 @@
 public class LC240_SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length; //rows
+        int n = matrix[0].length; //columns
+
+        //start from top right corner
         int i=0, j=n-1;
+
+        //run loop until we stay inside matrix boundaries
         while( i<=m-1 && j>=0){
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
-            else i++;
+            if(matrix[i][j]==target) return true;  // found the target
+            else if(matrix[i][j]>target) j--;   // move left (because values decrease to left)
+            else i++;    // move down (because values increase downward)
         }
-        return false;
+        return false;   //target not found
     }
 }
